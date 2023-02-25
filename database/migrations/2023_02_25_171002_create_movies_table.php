698e28caf4c7c8619fe03de75595625f79d550e5
Gabriel O <info.otero.gabriel@gmail.com>
2023-02-25 18:53:38 +0100

    Previous migration

--- database/migrations/2023_02_25_171002_create_movies_table.php
+++ database/migrations/2023_02_25_171002_create_movies_table.php
@@ -12,6 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('movies', function (Blueprint $table) {
+            $table->engine="InnoDB";
             $table->id();
             $table->bigInteger('genre_id')->unsigned();
             $table->integer('user_id')->index();
